d6ce81da8d27ef6b473327a6f4cf99a95eb2258f
CorreiaJV <correia.jv16@gmail.com>
2021-04-22 20:35:27 -0300

    Testes unitarios para requisiçao na API

--- test/user_services_test.dart
+++ test/user_services_test.dart
@@ -6,19 +6,11 @@ import '../lib/utils/services/user/user_services.dart';
 class MockitoExample extends Mock implements http.Client {}
 
 void main() {
-  test('Request Name from API', () async {
+  test('Request User API', () async {
     UserServices services = UserServices();
     final teste = await services.getUser(5);
     expect(teste.firstName, "U2");
-  });
-  test('Request Email from API', () async {
-    UserServices services = UserServices();
-    final teste = await services.getUser(5);
     expect(teste.email, "2@2.com");
-  });
-  test('Request Email from API', () async {
-    UserServices services = UserServices();
-    final teste = await services.getUser(5);
     expect(teste.account.cep, "2222222");
   });
 }
