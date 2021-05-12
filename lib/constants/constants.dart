be1d7c8f4e0a833d6f862f0423c01bd3e8a085e8
ciceroff <cicero.barrozo13@gmail.com>
2021-05-12 11:15:47 -0300

    Creating new menu page

---
+++ lib/constants/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const kSpacingUnit = 10;
+
+const kDarkPrimaryColor = Color(0xFF212121);
+
+final kTitleTextStyle = TextStyle(
+  fontSize: (10 * 1.7),
+  fontWeight: FontWeight.w600,
+);
+
+final kCaptionTextStyle = TextStyle(
+  fontSize: (10 * 1.3),
+  fontWeight: FontWeight.w100,
+);
+
+final kButtonTextStyle = TextStyle(
+  fontSize: (10 * 1.5),
+  fontWeight: FontWeight.w400,
+  color: kDarkPrimaryColor,
+);
--- lib/modules/screen/HomePage.dart
+++ lib/modules/screen/HomePage.dart
@@ -1,76 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:marca_spb/constants/menu_list_item.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: SideDrawer(),
       appBar: AppBar(
         title: Text('Marca Mutante'),
         backgroundColor: Colors.blue[400],
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Clique para gerar uma nova'),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, '/questionary'),
-              child: Text('Clica aqui'),
+       body: Column(
+              children: <Widget>[
+                SizedBox(height: 50),
+                //header,
+                Expanded(
+                  child: ListView(
+                    children: <Widget>[
+                      InkWell(
+                        child: MenuListItem(
+                          icon: LineAwesomeIcons.home,
+                          text: 'Home',
+                        ),
+                        onTap:() => Navigator.pushNamed(context, '/menu'),
+                      ),
+                      InkWell(
+                        child: MenuListItem(
+                          icon: LineAwesomeIcons.user,
+                          text: 'Perfil',
+                        ),
+                        onTap:() => Navigator.pushNamed(context, '/profile'),
+                      ),
+                      InkWell(
+                        child: MenuListItem(
+                          icon: LineAwesomeIcons.edit,
+                          text: 'Criar Marca',                      
+                        ),
+                        onTap:() => Navigator.pushNamed(context, '/questionary'),
+                      ),
+                      InkWell(
+                        child: MenuListItem(
+                          icon: LineAwesomeIcons.alternate_sign_out,
+                          text: 'Logout',
+                          hasNavigation: false,
+                        ),
+                        onTap:() => Navigator.of(context).pop(),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SideDrawer extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          DrawerHeader(
-            child: Center(
-              child: Text(
-                'Marca Mutante',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black, fontSize: 25),
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.blue[400],
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            onTap: () => {Navigator.pushNamed(context, '/menu')},
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Perfil'),
-            onTap: () => {Navigator.pushNamed(context, '/profile')},
-          ),
-          ListTile(
-            leading: Icon(Icons.border_color),
-            title: Text('Criar marca'),
-            onTap: () => {Navigator.pushNamed(context, '/questionary')},
-          ),
-          ListTile(
-            leading: Icon(Icons.border_color),
-            title: Text('Editar SpB'),
-            onTap: () => {Navigator.pushNamed(context, '/generate')},
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('Logout'),
-            onTap: () => Navigator.of(context).pop(),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
